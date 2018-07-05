850a3ce240c897d68fac1996586797d0d30350c2
Michael Wonng <michael@wonng.com>
2018-07-05 16:11:36 +1000

    add basic login function

--- pages/admin.js
+++ pages/admin.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import Router from 'next/router'
+import AuthService from '../utils/AuthService'
+import Button from '../components/form-components/Button'
+
+
+const Auth = new AuthService('http://localhost:3000')
 
 class Admin extends Component {
   constructor(props) {
@@ -13,28 +18,64 @@ class Admin extends Component {
   }
 
   componentDidMount() {
-    axios.defaults.headers.common['Authorization'] =  localStorage.getItem(process.env.TOKEN_KEY);
-    axios({
-      method: 'post',
-      url: 'http://localhost:3000/auth/token',
-      data: {sendToken: localStorage.getItem(process.env.TOKEN_KEY)}
-    })
-    .then(response => {
-      if (response.data.success) {
-        console.log("auth success")
-        this.setState({
-          isLoading: false,
-          isLogin: true,
+    let token = localStorage.getItem(process.env.TOKEN_KEY)
+    // axios.defaults.headers.common['Authorization'] =  localStorage.getItem(process.env.TOKEN_KEY);
+    // axios({
+    //   method: 'post',
+    //   url: 'http://localhost:3000/auth/token',
+    //   data: {sendToken: localStorage.getItem(process.env.TOKEN_KEY)}
+    // })
+    // .then(response => {
+    //   if (response.data.success) {
+    //     console.log("auth success",response.data)
+    //     this.setState({
+    //       isLoading: false,
+    //       isLogin: true,
+    //     })
+    //   } else {
+    //     console.log("failed")
+    //     this.setState({
+    //       isLoading: false,
+    //       isLogin: false
+    //     })
+    //     Router.push('/login')
+    //   }
+    // })
+
+
+
+    let isLoggedin =Auth.loggedIn()
+    if (isLoggedin) {
+      Auth.autoLogin(token)
+        .then(response => {
+          if (response.data.success) {
+            console.log("auth success")
+            this.setState({
+              isLoading: false,
+              isLogin: true,
+            })
+          } else {
+            console.log("failed")
+            this.setState({
+              isLoading: false,
+              isLogin: false
+            })
+            Router.push('/login')
+          }
         })
-      } else {
-        console.log("failed")
-        this.setState({
-          isLoading: false,
-          isLogin: false
+        .catch(err => {
+          Router.push('/login')
         })
-        Router.push('/login')
-      }
-    })
+    } else {
+      Router.push('/login')
+    }
+    console.log("exp?", Auth.isTokenExpired(localStorage.getItem(process.env.TOKEN_KEY)))
+    console.log("isLoggedin",isLoggedin)
+  }
+
+  logout() {
+    Auth.logout()
+    Router.push('/login')
   }
 
   render() {
@@ -46,7 +87,12 @@ class Admin extends Component {
     if (!this.state.isLoading && this.state.isLogin) {
       return (
         <div>
-          Admin page
+          <h2>Admin page</h2>
+          <Button
+            color="red"
+            onClick={this.logout}
+            text="Sign out"
+          />
         </div>
       );
     }
