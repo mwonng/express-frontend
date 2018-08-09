20dce6d0a14641ea3169e3a7b5b2d99a00a863ba
Michael Wonng <michael@wonng.com>
2018-08-10 02:46:56 +1000

    add mouse over and hold function

--- components/form-components/FlashMessage.js
+++ components/form-components/FlashMessage.js
@@ -1,76 +1,5 @@
-import styled, {keyframes} from 'styled-components'
+import styled from 'styled-components'
 import { darken  } from 'polished'
-
-// const theme = (props) => props.type ;
-const theme = {
-  warning: {
-    bgColor: '#fffaf3',
-    fontColor: '#794b02',
-    boxShadow: '0 0 0 1px #c9ba9b inset, 0 0 0 0 transparent'
-  },
-  success: {
-    bgColor: '#fcfff5',
-    fontColor: '#2c662d',
-    boxShadow: '0 0 0 1px #a3c293 inset, 0 0 0 0 transparent'
-  },
-  error: {
-    bgColor: '#fff6f6',
-    fontColor: '#9f3a38',
-    boxShadow: '0 0 0 1px #e0b4b4 inset, 0 0 0 0 transparent'
-  }
-}
-
-
-class FlashMessage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isShown: true,
-      isFading: false
-    }
-  }
-  componentDidMount() {
-    this.timer = setTimeout(
-      () => this.onClose(),
-      this.props.duration
-    );
-  }
-
-  onClose() {
-    this.setState({isShown:false})
-  }
-
-  onHold() {
-    // console.log("WHAT IS ON HOLDE");
-    clearTimeout(this.timer);
-  }
-
-  resumeTimer() {
-
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.timer);
-  }
-
-  render() {
-    const {isShown} = this.state
-    return (
-      <FlashContiner className={isShown?'show':'hide'}>
-        <FlashMsgWrapper
-          color={this.props.color}
-          onClick={this.props.onClick}
-          theme={this.props.type}
-          isShown={isShown}
-          onMouseEnter={this.onHold}
-          onMouseLeave={this.resumeTimer}
-          >{this.props.text}
-        </FlashMsgWrapper>
-      </FlashContiner>
-
-    );
-  }
-}
 
 const FlashContiner = styled.div`
   margin: 0;
@@ -96,6 +25,9 @@ const FlashContiner = styled.div`
     margin-top: -5rem;
     transition: margin-top .5s .5s;
   }
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const FlashMsgWrapper = styled.div`
@@ -105,7 +37,77 @@ const FlashMsgWrapper = styled.div`
   margin: .5rem auto;
   background: ${props => theme[props.theme].bgColor};
   color: ${props => theme[props.theme].fontColor};
-`
+`;
+
+const theme = {
+  warning: {
+    bgColor: '#fffaf3',
+    fontColor: '#794b02',
+    boxShadow: '0 0 0 1px #c9ba9b inset, 0 0 0 0 transparent'
+  },
+  success: {
+    bgColor: '#fcfff5',
+    fontColor: '#2c662d',
+    boxShadow: '0 0 0 1px #a3c293 inset, 0 0 0 0 transparent'
+  },
+  error: {
+    bgColor: '#fff6f6',
+    fontColor: '#9f3a38',
+    boxShadow: '0 0 0 1px #e0b4b4 inset, 0 0 0 0 transparent'
+  }
+};
+
+class FlashMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShown: true,
+    }
+    this.onClose = this.onClose.bind(this);
+    this.readyToFadeOut = this.readyToFadeOut.bind(this);
+    this.onHold = this.onHold.bind(this);
+  }
+  componentDidMount() {
+    this.readyToFadeOut();
+  }
+
+  readyToFadeOut() {
+    this.timer = setTimeout(
+      () => this.onClose(),
+      this.props.duration
+    );
+  }
+
+  onClose() {
+    this.setState({isShown:false})
+  }
+
+  onHold() {
+    clearTimeout(this.timer);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    const {isShown} = this.state
+    return (
+      <FlashContiner className={isShown?'show':'hide'}>
+        <FlashMsgWrapper
+          color={this.props.color}
+          onClick={this.props.onClick}
+          theme={this.props.type}
+          isShown={isShown}
+          onMouseEnter={this.onHold}
+          onMouseLeave={this.readyToFadeOut}
+          >{this.props.text}
+        </FlashMsgWrapper>
+      </FlashContiner>
+
+    );
+  }
+}
 
 FlashMsgWrapper.defaultProps = {
   theme: 'error',
